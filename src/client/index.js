0ea224e5ff8c66c3f4cc22f0415e7fe6c43b0798
Mehmet Turşucu <42400105+tursucu@users.noreply.github.com>
2020-04-24 10:30:30 +0300

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -6,13 +6,29 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { setContext } from 'apollo-link-context';
 import App from '../app/App';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4002/graphql',
+  credentials: 'same-origin',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = Cookies.get('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: process.env.API_ROOT,
-    credentials: 'same-origin',
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
 
